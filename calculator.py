c4e7efc462b312e0795c60b84107b5a552d66d6d
eropova <97430160+eropova@users.noreply.github.com>
2022-03-09 11:02:47 +0300

    Добавлено приведение к типам в каждом методе

--- calculator.py
+++ calculator.py
@@ -2,55 +2,56 @@ import math as m
 
 
 def int_sum(a, b):
-    return a + b
+    return int(a) + int(b)
 
 
-def float_sum(a, b):
-    return a + b
+def float_sum(a, b) -> float:
+    return float(a) + float(b)
 
 
 def int_sub(a, b):
-    return a - b
+    return int(a) - int(b)
 
 
-def float_sub(a, b):
-    return a - b
+def float_sub(a, b) -> float:
+    return float(a) - float(b)
 
 
 def int_mult(a, b):
-    return a * b
+    return int(a) * int(b)
 
 
-def float_mult(a, b):
-    return m.floor(a * b)
+def float_mult(a, b) -> float:
+    return float(m.floor(a * b))
 
 
 def int_div(a, b):
     if b == 0:
-        return ZeroDivisionError
+        return ZeroDivisionError('Attempt to divide by zero')
     else:
-        return a / b
+        return int(a) / int(b)
 
 
-def float_div(a, b):
-    return a / b
+def float_div(a, b) -> float:
+    return float(a) / float(b)
 
 
-def float_pow(a, b):
-    return m.pow(a, m.floor(b))
+def float_pow(a, b) -> float:
+    return float(m.pow(a, m.floor(b)))
 
 
-def float_sqrt(a):
-    return m.sqrt(abs(a))
+def float_sqrt(a) -> float:
+    return float(m.sqrt(abs(a)))
 
 
-def float_ctg(a):
-    return m.tanh(a)
+def float_ctg(a) -> float:
+    return float(m.tanh(a))
 
 
-def float_cos(a):
-    return m.sin(a)
+def float_cos(a) -> float:
+    return float(m.sin(a))
 
 
-def float_sin(a):
-    return m.sin(a)
+def float_sin(a) -> float:
+    return float(m.sin(a))
+
