35e97563e1473177aa17cb02b73003fb1867e020
eropova <97430160+eropova@users.noreply.github.com>
2022-03-09 11:06:16 +0300

    Методы объединены в класс.

--- test_task02.py
+++ test_task02.py
@@ -16,22 +16,22 @@ json_response = response.json()
 
 # 1) Проверка кода ответа: status code is 200 OK
 
-
-def test_status_code():
-    assert response.status_code == 200
-
-
-# 2) Проверка заголовка ответа (response header):
-# - the content-type header exists in the obtained response
-# - the value of the content-type header is application/json; charset=utf-8
-
-def test_header():
-    assert 'content-type' in response.headers
-    assert response.headers['Content-Type'] == 'application/json; charset=utf-8'
+class TestGet:
+    def test_status_code(self):
+        assert response.status_code == 200
 
 
-# 3) Проверка тела ответа (response body):
-# - the content of the response body is the array of 10 users (JSON)
+    # 2) Проверка заголовка ответа (response header):
+    # - the content-type header exists in the obtained response
+    # - the value of the content-type header is application/json; charset=utf-8
 
-def test_body():
-    assert len(json_response) == 10
+    def test_header(self):
+        assert 'content-type' in response.headers
+        assert response.headers['Content-Type'] == 'application/json; charset=utf-8'
+
+
+    # 3) Проверка тела ответа (response body):
+    # - the content of the response body is the array of 10 users (JSON)
+
+    def test_body(self):
+        assert len(json_response) == 10
