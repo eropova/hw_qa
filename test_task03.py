87ced745a1cf2914cdbab792fde071c9215e7f26
eropova <97430160+eropova@users.noreply.github.com>
2022-03-14 14:43:39 +0300

    Исправлено использование Mock

--- test_task03.py
+++ test_task03.py
@@ -2,23 +2,24 @@
 # Создайте аналогичный тест из п.3 предыдущего задания, но вместо отправки GET-запроса,
 # создайте Mock, возвращающий список из 10 пользователей.
 
-from unittest.mock import Mock
 
-with open('1.txt', 'r+') as file:
-    response = Mock(return_value=file)
+from unittest.mock import patch
 
-response.status_code.return_value = 200
-response.headers.return_value = 'application/json; charset=utf-8'
-response.json.return_value = 10
+
+with patch('test_task02.response') as mock:
+    mock_response = mock.return_value
+    mock_response.status_code.return_value = 200
+    mock_response.headers.return_value = 'application/json; charset=utf-8'
+    with open('1.txt', 'r+') as file:
+        mock_response.json.return_value = file.read()
+
 
 class TestMock:
     def test_status_code(self):
-        assert response.status_code() == 200
-
+        assert mock_response.status_code() == 200
 
     def test_header(self):
-        assert response.headers() == 'application/json; charset=utf-8'
-
+        assert mock_response.headers() == 'application/json; charset=utf-8'
 
     def test_body(self):
-        assert response.json() == 10
+        assert mock_response.json().count("'id'") == 10
